15fae83cb8e37877bf7692e5da7ef2f57f8cfd27
Binh20PMC <0306201216@caothang.edu.vn>
2023-04-21 14:20:33 +0700

    Update UI

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Map : MonoBehaviour
 {
--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,27 +5,34 @@ public class Zoom : MonoBehaviour
 {
     [SerializeField] RectTransform Time;
     [SerializeField] RectTransform ScoreAndChange;
+    [SerializeField] RectTransform rectTransform;
+    private Vector2 time;
+    private Vector2 scoreAndChange;
+    private float height;
+    private void Start()
+    {
+        time = Time.anchoredPosition;
+        scoreAndChange = ScoreAndChange.anchoredPosition;
+
+        height = rectTransform.rect.height;
+    }
     private void Update()
     {
-        Vector2 time = Time.anchoredPosition;
-        Vector2 scoreAndChange = ScoreAndChange.anchoredPosition;
-        if (Screen.width == 960)
-        {
-            time.x = -125;
-            scoreAndChange.x = 20;
-            Time.anchoredPosition = time;
-            ScoreAndChange.anchoredPosition = scoreAndChange;
-
-
-        }
-        else
+        if (height == 1080)
         {
             scoreAndChange.x = 60;
             time.x = -160;
             Time.anchoredPosition = time;
             ScoreAndChange.anchoredPosition = scoreAndChange;
         }
+        else if (height == 1200)
+        {
+            time.x = -125;
+            scoreAndChange.x = 20;
+            Time.anchoredPosition = time;
+            ScoreAndChange.anchoredPosition = scoreAndChange;
+        }
+        height = rectTransform.rect.height;
     }
-
 }
 
